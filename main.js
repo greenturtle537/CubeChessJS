d0066e973c1df4521304027bd94d7c98b3865de4
greenturtle537 <support@glitchtech.top>
2025-01-07 23:32:14 +0000

    Adjust canvas window height for 80x24 text grid support

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {CanvasWindow, TextWindow, KeyBehaviour, LaunchHook} from 'https://files.glitchtech.top/GE.js';
-const canvasWindow = new CanvasWindow(640, 480, main);
+const canvasWindow = new CanvasWindow(640, 192, main); // Allows for a 80x24 text grid with 8x8 characters
 
 function main() {
     return
