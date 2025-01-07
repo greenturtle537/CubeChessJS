5ad511a46819cf23f74d80b51c21d9600edd2456
greenturtle537 <support@glitchtech.top>
2025-01-07 23:28:33 +0000

    Add basic pages

---
+++ main.js
@@ -0,0 +1,6 @@
+import {CanvasWindow, TextWindow, KeyBehaviour, LaunchHook} from 'https://files.glitchtech.top/GE.js';
+const canvasWindow = new CanvasWindow(640, 480, main);
+
+function main() {
+    return
+}
