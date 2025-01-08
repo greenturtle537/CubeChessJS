fc43bdc3f582e16653fccd047b9f8d9e40f4eb21
greenturtle537 <support@glitchtech.top>
2025-01-08 00:48:27 +0000

    Add main menu functionality with text window display

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import {CanvasWindow, TextWindow, KeyBehaviour, LaunchHook} from 'https://files.glitchtech.top/GE.js';
+import {CanvasWindow} from 'https://files.glitchtech.top/GE.js';
 const canvasWindow = new CanvasWindow(640, 384, main); // Allows for a 80x24 text grid with 8x16 characters
 
 function main() {
-    return
+    textWindow = new TextWindow(640, 384, 0, 0, 0, 0, false, KeyBehaviour.getBehaviour("menu"));
+    mainMenu();
+}
+
+function mainMenu() {
+    //This is the main menu
+    textWindow.drawText("Main Menu", 288, 8);
+    textWindow.drawText("> Start", 0, 16);
+    textWindow.drawText("Load", 16, 32, 9);
+    textWindow.drawText(">", 0, 32, 0);
+    textWindow.drawText("> Options", 0, 48);
+    textWindow.drawText("> Exit", 0, 64);
 }
