0ae05319e079987a087a3c7f41ab1f01279a40b5
greenturtle537 <support@glitchtech.top>
2025-01-08 01:45:08 +0000

    Add console logging for chess pieces' positions in renderChessBoard function

--- main.js
+++ main.js
@@ -110,6 +110,7 @@ function renderChessBoard(chessWindow, chessBoard, chessPieces) {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             let chessPiece = chessPieces[chessBoard[i][j]];
+            console.log(`${chessPiece}  ${i}  ${j}`);
             chessWindow.drawText(chessPiece[0], i*24, j*48);
             chessWindow.drawText(chessPiece[1], i*24, j*48+8);
             chessWindow.drawText(chessPiece[2], i*24, j*48+16);
