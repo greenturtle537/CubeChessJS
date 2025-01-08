d426c85ac1c7c6f4344a6ba897988769929dce2a
greenturtle537 <support@glitchtech.top>
2025-01-08 07:01:27 +0000

    Fix chess piece rendering to correctly access sprite property

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ function initChessBoard(boardData) {
 function renderChessBoard(chessWindow, chessBoard, chessPieces) {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            let chessPiece = chessPieces[chessBoard[i][j]["piece"]["pieceName"]];["sprite"];
+            let chessPiece = chessPieces[chessBoard[i][j]["piece"]["pieceName"]]["sprite"];
             let tileColor = chessBoard[i][j]["tile"]["pattern"];
             let pieceColor = chessBoard[i][j]["piece"]["color"];
             let color = [tileColor, pieceColor];
