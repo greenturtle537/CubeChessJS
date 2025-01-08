c7901c8e8146ff441701e1bea3d14862ffab1931
greenturtle537 <support@glitchtech.top>
2025-01-08 01:13:42 +0000

    Fix menu navigation and update error message text size in main function

--- main.js
+++ main.js
@@ -9,38 +9,33 @@ function main() {
         let typingPositionX = 0;
         let typingPositionY = 0;
         let command = "";
-        let flag = 1;
         
-        console.log("KeyListener Loaded");
         document.addEventListener("keydown", function(event) {
             const key = event.key;
-    
             if (flag === 1) {
-                if  ((key === "w" || key === "s") && (menuPos != -1 && menuPos != 4)) {
+                if  ((key === "w" || key === "s") && (menuPos != -1 && menuPos != 3)) {
                     textWindow.drawText(">", 0, 0+(16*(menuPos+1)), 0);
                     console.log("drawn");
                 }
                 if ( key === "w" && menuPos != 0) {
                     menuPos--;
                     textWindow.drawText(">", 0, 0+(16*(menuPos+1)), 15);
-                } else if (key === "s" && menuPos != 3) {
+                } else if (key === "s" && menuPos != 2) {
                     menuPos++;
                     textWindow.drawText(">", 0, 0+(16*(menuPos+1)), 15);
                 } else if (key === "Enter") {
                     if (menuPos === 0) {
                         // Start
-                        textWindow.drawText("An Error Occured", 272, 0, 16);
+                        textWindow.drawText("An Error Occured", 272, 0, 12);
                         flag++;
                         textWindow.clearScreen();
                         introSeq();
                     } else if (menuPos === 1) {
-                        // Load
-                    } else if (menuPos === 2) {
                         // Options
-                        textWindow.drawText("An Error Occured", 272, 0, 16);
-                    } else if (menuPos === 3) {
+                        textWindow.drawText("An Error Occured", 272, 0, 12);
+                    } else if (menuPos === 2) {
                         // Exit
-                        textWindow.drawText("An Error Occured", 272, 0, 16);
+                        textWindow.drawText("An Error Occured", 272, 0, 12);
                     }
                 }
             } else if (flag === 2) {
@@ -84,11 +79,9 @@ function main() {
 
 function mainMenu() {
     //This is the main menu
-    textWindow.drawText("Main Menu", 288, 8);
+    textWindow.drawText("Main Menu", 288, 0);
     textWindow.drawText("> Play Chess", 0, 16);
-    textWindow.drawText("Load", 16, 32, 0);
-    textWindow.drawText(">", 0, 32, 0);
-    textWindow.drawText("> Options", 0, 48);
+    textWindow.drawText("> Options", 0, 32);
     textWindow.drawText("> Exit", 0, 64);
 }
 
